5ab0cf321c2b226d79b99286217cf9e547e55bd5
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2019-09-23 11:24:39 +0800

    更新错误

---
+++ fs/ramdisk/mkfs_cramfs_ramdisk.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ ! -n "$1" ] ;then
+    echo "usage: $0 ROOTFS_DIR RAMDISK_SIZE OUTPUT_DIR"
+    echo "please input ROOTFS_DIR"
+    exit 1
+fi
+
+if [ ! -n "$2" ] ;then
+    echo "usage: $0 ROOTFS_DIR RAMDISK_SIZE OUTPUT_DIR"
+    echo "please input RAMDISK_SIZE"
+    exit 1
+fi
+
+if [ ! -n "$3" ] ;then
+    echo "usage: $0 ROOTFS_DIR RAMDISK_SIZE OUTPUT_DIR"
+    echo "please input OUTPUT_DIR"
+    exit 1
+fi
+
+ROOTFS_DIR=$1
+RAMDISK_SIZE=$2
+OUTPUT_DIR=$3
+
+sudo chown -R root $ROOTFS_DIR
+sudo chgrp -R root $ROOTFS_DIR
+
+sudo dd if=/dev/zero of=ramdisk bs=1k count=$RAMDISK_SIZE
+sudo mkfs.cramfs $ROOTFS_DIR ramdisk
+
+sudo gzip --best ./ramdisk
+
+sudo chown -R `whoami` ramdisk.gz
+sudo chgrp -R `whoami` ramdisk.gz
+
+sudo mv ./ramdisk.gz $OUTPUT_DIR
